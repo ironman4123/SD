fd6065bd9cc5467f80d0a6a7360cfc047254d4fc
ironman4123 <143340746+ironman4123@users.noreply.github.com>
2023-08-27 21:07:13 +0530

    Add files via upload

---
+++ login/index.js
@@ -0,0 +1,8 @@
+// Get a reference to the button element by its ID
+var button = document.getElementById("myButton");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+    // Use the alert function to display a message when the button is clicked
+    alert("Login Successfully!");
+});
